e6aa3f036cb54502faf57eb6ee0f3586693af5e1
Brent Rusnell <blrusnell@gmail.com>
2019-05-07 11:26:25 -0500

    actually using the repo

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+// allows access to client folder
+app.use(express.static('client'));
+
+
+
+//app.get('/', (req, res) => res.send(' World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
