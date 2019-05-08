da11a76fb91f33e8bcf15d98f74d184e2c062c40
Brent Rusnell <blrusnell@gmail.com>
2019-05-08 11:16:11 -0500

    react, html, server setup

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
+
+export default App;
+
+
+ReactDOM.render('<App />' , document.getElementById('app'));
+  
