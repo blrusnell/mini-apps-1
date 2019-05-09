1e72a822dcc4e2311856e04db22b0f7b34558343
Brent Rusnell <blrusnell@gmail.com>
2019-05-09 12:11:08 -0500

    square component and app set up

--- challenge_4/client/src/components/app.jsx
+++ challenge_4/client/src/components/app.jsx
@@ -1,2 +1,26 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import Square from './square.jsx';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    };
+
+
+    render() {
+        return(
+            <div className="square">
+                <Square x={0} y={0}/>
+                <Square x={1} y={0}/>
+                <Square x={2} y={0}/>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+
 
 
