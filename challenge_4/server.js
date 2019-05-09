c9fc0128a3578783b0ed651e3bd158d7a3fa0174
Brent Rusnell <blrusnell@gmail.com>
2019-05-09 10:35:13 -0500

    setup webpack, server running, showing html on page

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3002
 
+//add bodyparser
+
+app.use(express.static('client/dist'));
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
